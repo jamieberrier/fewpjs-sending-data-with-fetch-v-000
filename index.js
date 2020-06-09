cb5eeae84e5e7972969b0406add6b5ad220ceeed
Jamie Berrier <berrier.jamie@gmail.com>
2020-06-09 19:45:51 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -41,17 +41,17 @@ function submitData(name, email) {
   };
   // 1. The destination URL
   return fetch("http://localhost:3000/users", configObj)
-    // Use a then() call to access the Response object and use its built in json() method to parse the contents of the body property.
-    .then(response => response.json())
-    // Use a second then() to access this newly converted object.
-    // From this object, find the new id and append this value to the DOM.
-    .then(object => {
-      document.body.innerHTML = object.id;
-    })
-    // When writing the callback function for your catch(),
-    // expect to receive an object on error with a property, `message`, containing info about what went wrong.
-    // Append this message to the DOM when catch() is called
-    .catch(function(error) {
-      document.body.innerHTML = error.message;
-    });
+  // Use a then() call to access the Response object and use its built in json() method to parse the contents of the body property.
+  .then(response => response.json())
+  // Use a second then() to access this newly converted object.
+  // From this object, find the new id and append this value to the DOM.
+  .then(object => {
+    document.body.innerHTML = object.id;
+  })
+  // When writing the callback function for your catch(),
+  // expect to receive an object on error with a property, `message`, containing info about what went wrong.
+  // Append this message to the DOM when catch() is called
+  .catch(function(error) {
+    document.body.innerHTML = error.message;
+  });
 }
